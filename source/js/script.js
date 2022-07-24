70d9fa4a85950ae27358976772ba00fdb14a0ea2
Andrei Asipchuk <marsel20002008@yandex.ru>
2022-07-24 21:24:16 +0300

    верстка проекта

--- source/js/script.js
+++ source/js/script.js
@@ -11,12 +11,8 @@ partTitles.forEach((element) => {
   element.addEventListener('click', () => {
     let sectionClass = '.' + element.dataset.name;
     let activePart = mainElement.querySelector(sectionClass);
-    let activePartChildren = activePart.children;
-    let activeHeight = 0;
-    for (var i = 0; i < activePartChildren.length; i++) {
-      activeHeight += activePartChildren[i].offsetHeight
-    };
-    activeHeight += 70;
+    let activeHeight = activePart.querySelector('.content').offsetHeight;
+    //activeHeight += 30;
     activePart.classList.toggle('js-active');
     activePart.style.cssText = `height: ${activeHeight}px;`;
     if ((currentPart !== 0) && (currentPart !== activePart)) {
@@ -32,6 +28,7 @@ partTitles.forEach((element) => {
   })
 });
 
+// для управления с клавиатуры, tab и enter
 partTitles.forEach((element) => {
   element.addEventListener('keydown', (evt) => {
     if (evt.code == 'Enter') {
