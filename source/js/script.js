25871d8bd443e53b3df2b2cc373786926d2fc42d
Andrei Asipchuk <marsel20002008@yandex.ru>
2022-07-25 20:38:57 +0300

    Исправление багов

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ let pageBody = document.querySelector('body');
 let mainElement = document.querySelector('.page-main');
 let partTitles = mainElement.querySelectorAll('.part__title');
 let currentPart = 0;
+let currentItem = 0;
 let activeHeight = 0;
 let galleryToggles = mainElement.querySelectorAll('.toggle__link');
 let myArtSection = mainElement.querySelector('.my-art');
@@ -17,10 +18,9 @@ partTitles.forEach((element) => {
   element.addEventListener('click', () => {
     let sectionClass = '.' + element.dataset.name;
     let activePart = mainElement.querySelector(sectionClass);
-    activeHeight = activePart.querySelector('.content').scrollHeight;
-    // activeHeight += 20;
+    // проброс данных в css-переменную
+    document.documentElement.style.setProperty('--heightContainer', `${activePart.scrollHeight}`);
     activePart.classList.toggle('js-active');
-    activePart.style.cssText = `height: ${activeHeight}px;`;
     if ((currentPart !== 0) && (currentPart !== activePart)) {
       currentPart.classList.toggle('js-active');
       currentPart.style.cssText = '';
@@ -30,6 +30,16 @@ partTitles.forEach((element) => {
       activePart.style.cssText = '';
     } else {
       currentPart = activePart;
+    };
+    // при закрывании "Мое творчество" все галереи схлопываются
+    if (currentItem !== 0) {
+      if (currentItem.classList.contains('js-toggle__item')) {
+        currentItem.classList.remove('js-toggle__item')
+      };
+      if (currentItem.querySelector('.toggle__link').classList.contains('js-toggle__link')) {
+        currentItem.querySelector('.toggle__link').classList.remove('js-toggle__link')
+      };
+      currentItem = 0;
     };
   })
 });
@@ -46,12 +56,21 @@ partTitles.forEach((element) => {
 // галерея
 galleryToggles.forEach((element) => {
   element.addEventListener('click', () => {
+    // проброс данных в css-переменную
+    document.documentElement.style.setProperty('--heightContainer', 'auto');
     let activeItem = element.parentNode; // элемент <li> где кликнули кнопку
-    // let className = `gallery__${element.dataset.galleryLink}`;
-    element.classList.toggle('js-toggle__link');
-    activeItem.classList.toggle('js-toggle__item');
-    activeHeight = myArtSection.querySelector('.content').scrollHeight;
-    myArtSection.style.cssText = `height: ${activeHeight}px;`;
-    console.log(activeHeight);
+    if ((currentItem !== 0) && (currentItem !== activeItem)) {
+      currentItem.classList.toggle('js-toggle__item');
+      currentItem.querySelector('.toggle__link').classList.toggle('js-toggle__link');
+    };
+    if (currentItem == activeItem) {
+      currentItem = 0;
+      element.classList.toggle('js-toggle__link');
+      activeItem.classList.toggle('js-toggle__item');
+    } else {
+      element.classList.toggle('js-toggle__link');
+      activeItem.classList.toggle('js-toggle__item');
+      currentItem = activeItem;
+    };
   });
 });
