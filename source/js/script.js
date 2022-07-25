512c7f14d467c36a5e2c8af5c4b75061d57ad3dd
Andrei Asipchuk <marsel20002008@yandex.ru>
2022-07-25 15:50:40 +0300

    Работа над галереей

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,24 @@
-// lightGallery(document.querySelector('.gallery'), {
-//   plugins: [lgZoom],
-// });
-
 let pageBody = document.querySelector('body');
 let mainElement = document.querySelector('.page-main');
 let partTitles = mainElement.querySelectorAll('.part__title');
 let currentPart = 0;
+let activeHeight = 0;
 let galleryToggles = mainElement.querySelectorAll('.toggle__link');
+let myArtSection = mainElement.querySelector('.my-art');
 let galleryContainers = document.querySelectorAll('.js-gallery');
+
+galleryContainers.forEach((section) => {
+  lightGallery(section.querySelector('.gallery'), {
+    plugins: [lgZoom],
+    cancelable: false,
+  });
+});
 
 partTitles.forEach((element) => {
   element.addEventListener('click', () => {
     let sectionClass = '.' + element.dataset.name;
     let activePart = mainElement.querySelector(sectionClass);
-    let activeHeight = activePart.querySelector('.content').scrollHeight;
+    activeHeight = activePart.querySelector('.content').scrollHeight;
     // activeHeight += 20;
     activePart.classList.toggle('js-active');
     activePart.style.cssText = `height: ${activeHeight}px;`;
@@ -43,14 +48,11 @@ partTitles.forEach((element) => {
 galleryToggles.forEach((element) => {
   element.addEventListener('click', () => {
     let activeItem = element.parentNode; // элемент <li> где кликнули кнопку
-    let className = `gallery__${element.dataset.galleryLink}`;
+    // let className = `gallery__${element.dataset.galleryLink}`;
+    element.classList.toggle('js-toggle__link');
     activeItem.classList.toggle('js-toggle__item');
-    // activeItem.querySelector('.js-gallery').classList.toggle('js-gallery-visible');
-    // galleryContainers.forEach((item) => {
-    //   if (item.classList.contains(className)) {
-    //     item.style.cssText = 'position: fixed;top: 0;left: 0;z-index: 5;height: 100vh;background-color: rgb(169 14 14 / 64%);';
-    //     // pageBody.style.overflow = 'hidden';
-    //   };
-    // });
+    activeHeight = myArtSection.querySelector('.content').scrollHeight;
+    myArtSection.style.cssText = `height: ${activeHeight}px;`;
+    console.log(activeHeight);
   });
 });
